c364641fd9e2ecde064d669ebf466eac35bbd707
Douglas <dcvweb123@gmail.com>
2023-05-05 11:00:02 -0400

    Section 5 qworking on

--- Section 5/app.js
+++ Section 5/app.js
@@ -1,13 +1,18 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req, res, next) => {
-	res.send('<h1>Add product page!</h1>');
-});
+app.use(bodyParser.urlencoded());
 
-app.use('/', (req, res, next) => {
-	res.send('<h1>Hello from express!</h1>');
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
