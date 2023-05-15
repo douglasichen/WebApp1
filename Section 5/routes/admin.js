60a91b5349f167a409a041cc008cf32fa892ad8e
dch23 <dcvweb123@gmail.com>
2023-05-15 16:02:05 -0400

    new stuff

--- Section 5/routes/admin.js
+++ Section 5/routes/admin.js
@@ -3,6 +3,7 @@ const path = require('path');
 const rootDir = require('../util/path');
 
 const express = require('express');
+
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
